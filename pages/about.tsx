597cba766bd5e05b484e02bc8665e3b1430a39d1
Andrii_Frolov <andrii.rolov@pearson.com>
2023-07-06 22:12:25 +0300

    test commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,6 @@ import { Typography } from "../components/Typography"
 export default function AboutPage() {
     const text = 'About page 3'
 
-
     return (
         <Typography text={text} />
     )
